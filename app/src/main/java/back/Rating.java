818cf51807722b25f8ea1c50a0c30d45c8a2b99c
l-beneteau <beneteau.laurine@gmail.com>
2024-03-01 10:06:05 +0100

    getters BookEntity, creation enum Ratin

---
+++ app/src/main/java/back/Rating.java
@@ -0,0 +1,5 @@
+package back;
+
+public enum Rating {
+
+}
--- app/src/main/java/entity/BookEntity.java
+++ app/src/main/java/entity/BookEntity.java
@@ -1,7 +1,24 @@
 package entity;
 
 
-
+import jakarta.persistence.Entity;
+import java.util.Date;
+import lombok.Getter;
+@Entity
 public class BookEntity {
+    @Getter private int id;
+    @Getter private String title;
+    @Getter private AuthorEntity author;
+    @Getter private String series;
+    @Getter private int year;
+    @Getter private String genre;
+    @Getter private boolean french;
+    @Getter private boolean library;
+    @Getter private int pages;
+    @Getter private boolean readed;
+    @Getter private Date started;
+    @Getter private Date ended;
+    private int rating;
+    @Getter private String notes;
 
 }
