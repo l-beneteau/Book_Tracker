89aec1068893d62e95daf7b303efdd8ffe027b6b
l-beneteau <beneteau.laurine@gmail.com>
2024-03-01 10:31:54 +0100

    class BookEntity.java lombok

--- app/src/main/java/entity/BookEntity.java
+++ app/src/main/java/entity/BookEntity.java
@@ -1,24 +1,32 @@
 package entity;
 
 
+import back.Rating;
+import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import java.util.Date;
-import lombok.Getter;
+
+import lombok.*;
+
 @Entity
+@Getter@Setter
+@AllArgsConstructor@RequiredArgsConstructor
 public class BookEntity {
-    @Getter private int id;
-    @Getter private String title;
-    @Getter private AuthorEntity author;
-    @Getter private String series;
-    @Getter private int year;
-    @Getter private String genre;
-    @Getter private boolean french;
-    @Getter private boolean library;
-    @Getter private int pages;
-    @Getter private boolean readed;
-    @Getter private Date started;
-    @Getter private Date ended;
-    private int rating;
-    @Getter private String notes;
+    @NonNull private int id;
+    @NonNull private String title;
+    @NonNull private AuthorEntity author;
+    private String series;
+    private int year;
+    private String genre;
+    private boolean french;
+    private boolean library;
+    private int pages;
+    private boolean readed;
+    private Date started;
+    private Date ended;
+    @Convert(converter = RatingConverter.class)
+    private Rating rating;
+    private String notes;
+
 
 }
