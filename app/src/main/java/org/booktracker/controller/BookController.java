d32e2c8caf7777a2203e88ee50df2ef4cea11502
l-beneteau <beneteau.laurine@gmail.com>
2024-03-06 14:00:24 +0100

    BookService.java : method findBookById

--- app/src/main/java/org/booktracker/controller/BookController.java
+++ app/src/main/java/org/booktracker/controller/BookController.java
@@ -1,20 +1,24 @@
 package org.booktracker.controller;
 
 import org.booktracker.entity.BookEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.booktracker.service.BookService;
+
 @RestController
 @RequestMapping("book")
 public class BookController {
+
+    @Autowired
+    BookService bookService;
     @GetMapping(value = "/{id}", produces = "application/json")
     public BookEntity getBook(@PathVariable int id) {
-        return findBookById(id);
+        return bookService.findBookById(id);
     }
 
-    private BookEntity findBookById(int id) {
-        return null;
-    }
+
 }
--- app/src/main/java/org/booktracker/service/BookService.java
+++ app/src/main/java/org/booktracker/service/BookService.java
@@ -1,4 +1,17 @@
 package org.booktracker.service;
 
+import org.booktracker.entity.BookEntity;
+import org.booktracker.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookService {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    public BookEntity findBookById(int id) {
+        return bookRepository.findById(id);
+    }
 }
