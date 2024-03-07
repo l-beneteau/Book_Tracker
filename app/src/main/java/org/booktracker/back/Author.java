ba071ba1839a5529feda365a66e24668384815b3
l-beneteau <beneteau.laurine@gmail.com>
2024-03-07 09:41:04 +0100

    Author.java attributes + constructor

--- app/src/main/java/org/booktracker/back/Author.java
+++ app/src/main/java/org/booktracker/back/Author.java
@@ -1,4 +1,30 @@
 package org.booktracker.back;
 
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Author {
+    @NonNull
+    private Integer author_id;
+    @NonNull private String name;
+
+    private final Set<Book> books = new HashSet<>();
+
+    public Author(@NonNull Integer author_id, @NonNull String name) {
+        this.author_id = author_id;
+        this.name = name;
+    }
+
+    public Author(@NonNull Integer author_id, @NonNull String name, Set<Book> books) {
+        this.author_id = author_id;
+        this.name = name;
+    }
+
+
 }
